79364fbe140bdd0593c8430658a0d07611919237
y-007 <nakaiy@hotmail.com>
2026-04-29 15:58:49 -0400

    add sitemap

--- travel_website/vite.config.js
+++ travel_website/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.gohanworld.com'
+    })
+  ]
 })
